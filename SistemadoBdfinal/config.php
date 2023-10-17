43dfcba8f9f649c730e101fc459bc30dfaf52dce
Henrique20o <132854942+Robertinhou@users.noreply.github.com>
2023-10-17 14:53:14 -0300

    site
    site

--- SistemadoBdfinal/config.php
+++ SistemadoBdfinal/config.php
@@ -2,7 +2,7 @@
     $dbHost = 'Localhost';
     $dbUser = 'root';
     $dbPassword = '';
-    $dbName = 'ecommerce';
+    $dbName = 'BDAPI';
 
     $conexao = new mysqli($dbHost, $dbUser, $dbPassword, $dbName)
 
