576d2ab017942875557fb7cd76b3581edce391f0
Henrique20o <132854942+Robertinhou@users.noreply.github.com>
2023-10-19 16:19:59 -0300

    conferindo senha
    conferindo senha

--- Site/cadastro.php
+++ Site/cadastro.php
@@ -9,12 +9,19 @@ $senha = $_POST['senha'];
 
 include_once("connections/config.php");
 $select = mysqli_query($conexao, "SELECT * FROM usuarios WHERE email = '$email' ");
+$selectsenha = mysqli_query($conexao, "SELECT * FROM usuarios WHERE senha = '$senha' ");
 if (mysqli_num_rows($select)>0){
     echo "Essa conta já existe!";
 } else{
     $inserir = mysqli_query($conexao, "INSERT INTO usuarios (nome, email, senha) VALUES ( '$nome', '$email', '$senha')");
     header("Location: Login.html");
 }
+
+
+
+
+
+
 }
 
 ?>
