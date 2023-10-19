228d831f4f8891447f37181cb9f1a024ae32df05
ViniciusKendy17 <135672206+ViniciusKendy17@users.noreply.github.com>
2023-10-19 14:31:18 -0300

    daily update

--- Site/src/js/page_transition.js
+++ Site/src/js/page_transition.js
@@ -1,5 +1,6 @@
-import React from 'react';
+function changepage(){
+    window.location.href = "index.html";
 
-function openCustomPopup() {
-    var features = "width=400,height=300,top=100,left=100,toolbar=no,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no";
-    window.open("popup-content.html", "Custom Pop-up", features)};
+
+
+}
