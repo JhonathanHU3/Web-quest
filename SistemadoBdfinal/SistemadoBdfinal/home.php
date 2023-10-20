5d63c5205aaaf3c869b887593b4d53f5ae7618a9
Henrique20o <137103470+Henrique20o@users.noreply.github.com>
2023-10-20 17:51:24 -0300

    Criação das sessoes

---
+++ SistemadoBdfinal/SistemadoBdfinal/home.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.php">
+</head>
+<body class="body1">
+    <main>
+    <nav class="navbar">
+        <ul>
+            
+            <a href="cadastro_produtos.php">Cadastrar produto</a>
+            <a href="cadastro_fornecedor.php">Cadastrar Fornecedor</a>
+            <a href="Comprar.php">Comprar</a>
+        </ul>
+</nav>
+</html>
+
---
+++ SistemadoBdfinal/Site/Login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+if (isset($_POST['submit'])) {
+    $email = $_POST['email1'];
+    $senha = $_POST['senha1'];
+
+    include_once("connections/config.php");
+
+    $verificar = "SELECT email, senha, nome FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+    $result = $conexao->query($verificar);
+
+    if ($result && $result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $_SESSION['nome'] = $row['nome'];
+        header("Location: intro.php");
+        exit(); 
+    } else {
+        echo "Conta ou senha incorretos! <a href='Login.html'>Voltar</a>";
+    }
+}
+?>
+
+
