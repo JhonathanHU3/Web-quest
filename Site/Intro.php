0dc23ee096128c3313233588973a4a0b031056e2
Henrique20o <137103470+Henrique20o@users.noreply.github.com>
2023-10-21 13:50:58 -0300

    fds

--- Site/Intro.php
+++ Site/Intro.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/header.css">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/style/intro.css">
     <title>Introdução</title>
 </head>
 <body>
