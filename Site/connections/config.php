a865a49940b175b695c4449caed6a79501d7ab93
Jhonathan Pimenta <jhonathanscp@gmail.com>
2024-01-05 01:57:33 -0300

    removing credentials

--- Site/connections/config.php
+++ Site/connections/config.php
@@ -2,6 +2,6 @@
 $options = array(
     PDO::MYSQL_ATTR_SSL_CA => 'DigiCertGlobalRootCA.crt.pem'
 );
-$pdo = new PDO('mysql:host=api-webquest-teste01-server2.mysql.database.azure.com;port=3306;dbname=bdapi', 'qrlvyvqjpp', '114810AOWJI2F2F1$', $options);
+$pdo = new PDO('mysql:host=$host;port=3306;dbname=bdapi', $user, $password, $options);
 
 ?>
